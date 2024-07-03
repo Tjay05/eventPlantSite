eacecad64cea36b3b6445aab382688d87cd36023
Tjay05 <tjayolatayo@gmail.com>
2024-07-03 22:15:09 +0100

    Format code setup

--- src/assets/pages/About.jsx
+++ src/assets/pages/About.jsx
@@ -8,7 +8,7 @@ const AboutUs = () => {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
           nobis atque molestias perferendis maiores fugiat doloribus eos,
           tempore iure rerum omnis unde a autem veritatis, vitae odio? Excepturi
-          rem quis animi, repellat explicabo asperiores et delectus dicta,
+          rem quis animi, repellat asperiores et delectus dicta,
           laboriosam fugiat ullam.
         </p>
       </section>
