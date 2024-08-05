d345b085e5aeeff04ec52245e338b245671ca461
Tjay05 <tjayolatayo@gmail.com>
2024-08-05 12:10:44 +0100

    Add contact styles

--- src/assets/pages/Contact.jsx
+++ src/assets/pages/Contact.jsx
@@ -1,34 +1,40 @@
 const Contact = () => {
   return (
     <>
-      <section>
-        <h2>CONTACT US</h2>
-        <article className="card">
-          <h3>Address</h3>
-          <p>Anywhere, any city, 44533</p>
-        </article>
-        <article className="card">
-          <h3>Call Us</h3>
-          <p>Call us: +234700000000</p>
-        </article>
-        <article className="card">
-          <h3>Mail Us</h3>
-          <p>info@chochocho.com</p>
-        </article>
+      <section id="Contact">
+        <div className="container">
+          <h2>CONTACT US</h2>
+          <div className="contacts-container">
+            <article className="card">
+              <h3>Address</h3>
+              <p>Anywhere, any city, 44533</p>
+            </article>
+            <article className="card">
+              <h3>Call Us</h3>
+              <p>Call us: +234700000000</p>
+            </article>
+            <article className="card">
+              <h3>Mail Us</h3>
+              <p>info@chochocho.com</p>
+            </article>
+          </div>
+        </div>
       </section>
-      <section>
-        <h2>MAIL US</h2>
-        <div className="contact-container">
-          <form>
-            <div className="">
-              <input placeholder="Name" type="text" />
-              <input placeholder="Email" type="email" />
-            </div>
-            <input placeholder="Subject" type="text" />
-            <textarea name="" placeholder="Message" id="" cols="30" rows="10"></textarea>
-            <button>Send</button>
-          </form>
-          <iframe src="" frameborder="0"></iframe>
+      <section id="Mail">
+        <div className="container">
+          <h2>MAIL US</h2>
+          <div className="contact-container">
+            <form>
+              <div className="">
+                <input placeholder="Name" type="text" />
+                <input placeholder="Email" type="email" />
+              </div>
+              <input placeholder="Subject" type="text" />
+              <textarea name="" placeholder="Message" id="" cols="30" rows="10"></textarea>
+              <button>Send</button>
+            </form>
+            {/* <iframe src="" frameborder="0"></iframe> */}
+          </div>
         </div>
       </section>
     </>
